60617645ec9eb9c197d021b0aaf609d99da828e3
Martyn Lewis <Martyn-Lewis@users.noreply.github.com>
2017-02-20 20:37:55 +0000

    Pre-emptive support for indexes

--- src/main/scala/datatypes/Database.scala
+++ src/main/scala/datatypes/Database.scala
@@ -1,14 +1,21 @@
 package datatypes
 
-class Database {
+class Database(val enabled_indexes: Boolean) {
   var elements: List[Taggable] = List.empty
   var indexes: collection.mutable.Map[String, Int] = collection.mutable.Map[String, Int]()
+  var index_records: collection.mutable.Map[String, List[Taggable]] = collection.mutable.Map[String, List[Taggable]]()
 
   def add_element(element: Taggable): Unit = {
     elements ::= element
     element.tags foreach ((x) => {
       if(indexes.contains(x)) indexes(x) += 1
       else indexes(x) = 1
+
+      if(enabled_indexes) {
+        if (!index_records.contains(x))
+          index_records(x) = List.empty
+        index_records(x) ::= element
+      }
     })
   }
 
--- src/main/scala/datatypes/DatabasePool.scala
+++ src/main/scala/datatypes/DatabasePool.scala
@@ -4,7 +4,7 @@ class DatabasePool {
   var pools: collection.mutable.Map[String, Database] = collection.mutable.Map[String, Database]()
 
   def create_pool(name: String): Database = {
-    if(!pools.contains(name)) pools += name -> new Database()
+    if(!pools.contains(name)) pools += name -> new Database(true)
     get_pool(name)
   }
   def get_pool = pools(_)
